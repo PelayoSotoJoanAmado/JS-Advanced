1b7b5816bb184a8b165edd0e878565432adbd838
HuamaniChampeAlexander <alexbmpiscis@gmail.com>
2026-04-30 11:45:45 -0500

    Update root route response message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("API REST de productos funcionando correctamente");
 });
 
 app.get('/hola', (req, res) =>{
